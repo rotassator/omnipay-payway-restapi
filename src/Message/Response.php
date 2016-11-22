06514d8809d3462f88324406aa5dc4768b9a6f11
Steve Dixon <steve@solutionsoutsourced.com.au>
2016-11-22 14:48:53 +1100

    FEAT: fill out Response object

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -5,39 +5,129 @@
 namespace Omnipay\PaywayRest\Message;
 
 use Omnipay\Common\Message\AbstractResponse;
-use Omnipay\Common\Message\ResponseInterface;
 
 /**
- * PayWayRest Response
+ * PaywayRest Response
  *
+ * Response class for all PaywayRest requests
  * @see \Omnipay\PaywayRest\Gateway
  */
-class Response extends AbstractResponse implements ResponseInterface
+class Response extends AbstractResponse
 {
+    /** @var string Request ID */
+    protected $requestId = null;
+
     /**
      * Is the transaction successful?
      * @return boolean True if successful
      */
     public function isSuccessful()
     {
-        return !isset($this->data['message']);
+        return $this->getData('status') && in_array($this->getData('status'), array(
+            'approved',
+            'approved*',
+        ));
     }
 
     /**
-     * Get response error message
-     * @return string Error message
+     * Get Transaction ID
+     * @return string|null
+     */
+    public function getTransactionId()
+    {
+        return $this->getData('transactionId');
+    }
+
+    /**
+     * Get Customer Number
+     * @return string|null
+     */
+    public function getCustomerNumber()
+    {
+        return $this->getData('customerNumber');
+    }
+
+    /**
+     * Get Contact details
+     * @return array Customer contact
+     */
+    public function getContact()
+    {
+        return $this->getData('contact');
+    }
+
+    /**
+     * Get response data, optionally by key
+     * @param  string       $key Data array key
+     * @return string|array      Response data item or all data if no key specified
+     */
+    public function getData($key = null)
+    {
+        if ($key) {
+            return isset($this->data[$key]) ? $this->data[$key] : null;
+        }
+        return $this->data;
+    }
+
+    /**
+     * Get error data from response
+     * @param  string       $key Optional data key
+     * @return string|array      Response error item or all data if no key
+     */
+    public function getErrorData($key = null)
+    {
+        if ($this->isSuccessful()) {
+            return null;
+        }
+        // get error data
+        $data = $this->getData('data');
+        if ($key) {
+            return isset($data[$key]) ? $data[$key] : null;
+        }
+        return $data;
+    }
+
+    /**
+     * Get error message from the response
+     * @return string|null Error message or null if successful
      */
     public function getMessage()
     {
-        return !$this->isSuccessful() ? $this->data['message'] : '';
+        return $this->getErrorData('message');
     }
 
     /**
-     * Get response data
-     * @return array Array of data
+     * Get field name in error from the response
+     * @return string|null Error message or null if successful
      */
-    public function getData()
+    public function getErrorFieldName()
     {
-        return $this->data;
+        return $this->getErrorData('fieldName');
+    }
+
+    /**
+     * Get field value in error from the response
+     * @return string|null Error message or null if successful
+     */
+    public function getErrorFieldValue()
+    {
+        return $this->getErrorData('fieldValue');
+    }
+
+    /**
+     * @return string
+     */
+    public function getRequestId()
+    {
+        return $this->requestId;
+    }
+
+    /**
+     * Set request id
+     * @return AbstractRequest provides a fluent interface.
+     */
+    public function setRequestId($requestId)
+    {
+        $this->requestId = $requestId;
     }
 }
